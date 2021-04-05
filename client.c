571d0adfa55a0bbcca7c733bc8e84259f375753b
sliv2001 <ivan.sladkov@yandex.ru>
2021-04-05 18:34:52 +0300

    Added client module.
    It contains some funcs for TCP-connected sockets.
    UDP is to be added later.

--- client.c
+++ client.c
@@ -1,7 +1,10 @@
 #include "client.h"
 
-int establish_TCP(char* str_addr){
+int sfd = -1;
+
+int establish_TCP(const char* str_addr){
 	struct sockaddr_in addr;
+	struct in_addr a;
 	sfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sfd<0){
 		pr_err("Couldnot create socket");
@@ -10,12 +13,57 @@ int establish_TCP(char* str_addr){
 	memset(&addr, 0, sizeof(addr));
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(PORT);
+	a.s_addr = inet_addr(str_addr);
+	if (a.s_addr==INADDR_NONE){
+		pr_err("Wrong IP address: %s", str_addr);
+		exit(-1);
+	}
+	addr.sin_addr = a;
+	if (connect(sfd, (struct sockaddr*)&addr, sizeof(addr))<0){
+		pr_err("Couldnot connect to socket %s", str_addr);
+		exit(-1);
+	}
+	return 0;
+}
+
+int sendData(char* buf){
+	int i, n, wrong=0;
+	char small_buf[BASIC_STRLEN];
+	memset(small_buf, 0, BASIC_STRLEN);
+	for (i=0; i<BUFSZ-BASIC_STRLEN; i+=BASIC_STRLEN){
+		if (send(sfd, buf+i, BASIC_STRLEN, 0)<0)
+			wrong=1;
+	}
+	i = n;
+	for (i=BUFSZ-i; i<BUFSZ; i++){
+		small_buf[i]=buf[n+i];
+	}
+	if (send(sfd, small_buf, BASIC_STRLEN, 0)<0||wrong==1){
+		pr_err("Couldnot send data");
+		return -1;
+	}
 	return 0;
 }
 
 int main(int argc, char** argv){
+	char buf[BUFSZ];
+	int i;
+	if (log_init_fd(STDERR_FILENO)<0)
+		return -1;
 	if (argc<3)
 		pr_err("Wrong args");
 	establish_TCP(argv[argc-1]);
+	while (1){
+		if (read(STDIN_FILENO, &buf, BUFSZ)<0){
+			pr_err("Couldnot read stdin");
+			return -1;
+		}
+		for (i=0; i<BUFSZ-1; i++)
+			if (buf[i]=='\n'){
+				buf[i+1]=0;
+				break;
+			}
+		sendData(buf);
+	}
 	return 0;
 }
--- client.h
+++ client.h
@@ -1,11 +1,16 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <arpa/inet.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-#include "logger.h"
 #include "utils.h"
+#define LOG_PATH ""
+#include "logger.h"
 
 #define PORT 32001
-
+#define BASIC_STRLEN 128
 int establish_TCP(const char*);
+
+int sendData(char*);
