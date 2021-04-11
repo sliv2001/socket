f361ed11aab1133fc27800fb35010e75df1a93b2
sliv2001 <ivan.sladkov@yandex.ru>
2021-04-11 21:39:00 +0300

    Logger debug.
    Logger is now reliable.

--- logger.h
+++ logger.h
@@ -8,15 +8,15 @@
 #include <errno.h>
 
 #define pr_err(fmt, ...) \
-pr_log_level(LOG_ERR, "%s: %d" fmt, __FILE__, __LINE__, \
+pr_log_level(LOG_ERR, "%s: %d " fmt, __FILE__, __LINE__,\
 ##__VA_ARGS__);
 
 #define pr_info(fmt, ...) \
-pr_log_level(LOG_INFO, "%s: %d" fmt, __FILE__, __LINE__, \
+pr_log_level(LOG_INFO, "%s: %d " fmt, __FILE__, __LINE__, \
 ##__VA_ARGS__);
 
 #define pr_warn(fmt, ...) \
-pr_log_level(LOG_WARN, "%s: %d" fmt, __FILE__, __LINE__, \
+pr_log_level(LOG_WARN, "%s: %d " fmt, __FILE__, __LINE__, \
 ##__VA_ARGS__);
 
 #define BUFSZ 4096
@@ -57,18 +57,24 @@ int log_init_fd(int fd){
 /*date pid file-line log-level*/
 int pr_log_level(int level, char* fmt, ...){
 	char strlevel[5];
+	char date[128];
 	va_list params;
+	char* i;
 	time_t t = time(NULL);
 	if (log_fd==-1)
 		log_init(NULL);
 	if (level==0) sprintf(strlevel, "%s", "err");
 	if (level==1) sprintf(strlevel, "%s", "warn");
 	if (level==2) sprintf(strlevel, "%s", "info");
-	snprintf(buf, BUFSZ, "%s %d %s", ctime(&t), 
-		getpid(), strlevel);
+	strcpy(date, ctime(&t));
+	if ((i=strstr(date, "\n"))>=0)
+		*i = 0;
+	snprintf(buf, BUFSZ, "%s %d %s", date, getpid(), strlevel);
+	va_start(params, fmt);
 	vsnprintf(buf+strlen(buf), BUFSZ-strlen(buf), fmt, params);
-	if (level==0)
-		snprintf(buf+strlen(buf), BUFSZ-strlen(buf), "%s", strerror(errno));
+	va_end(params);
+	if (level==0&&errno!=0)
+		snprintf(buf+strlen(buf), BUFSZ-strlen(buf), ": %s", strerror(errno));
 	if (write(log_fd, buf, strlen(buf))<0)
 		return -1;
 	return 0;
